7b3ce77c49c9f0a7eba524c6041840ee8464a9eb
Sathwik <sathwikdec@gmail.com>
2018-07-11 15:10:20 -0400

    modify sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -1,13 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 void main()
 {
-	int a[20], i, j,n,c=0;                        // 'c' is the temporary variable for bubble sorting
+	// 'c' is the temporary variable for bubble sorting
+	int a[20], i, j,n,c=0;
 	cout << "Enter the number of elements you want to sort"<<endl;
 	cin >> n;
 	cout << "Enter the " << n << " elements" << endl;
-	for (i = 0; i < n; i++)cin >> a[i];
+	for (i = 0; i < n; i++)
+		cin >> a[i];
 	for (i = 0; i < n; i++)                       
 	{
 		for (j = i+1; j < n; j++)
@@ -20,8 +22,10 @@ void main()
 			}
 		}
 	}
+	// Print output
 	cout << "Numbers in ascending order : ";
-	for (i = 0; i < n; i++)cout << " "<<a[i];
+	for (i = 0; i < n; i++)
+		cout << " "<<a[i];
 	cin >> i;
 	
 	cout << "Done" <<endl;
